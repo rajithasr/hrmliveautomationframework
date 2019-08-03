a525077a929377c5c441ef6e2aa3c2b28cf1a5a7
home <home@192.168.0.6>
2019-08-03 15:56:54 +0530

    first commit

---
+++ hrmliveautomationframework/src/main/java/com/hrmautomation/pages/App.java
@@ -0,0 +1,13 @@
+package com.hrmautomation.pages;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
